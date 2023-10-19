e1bbd25f1ef7b70638c8d45946a1c018b835f3d9
Morad Kharraz Senhaji <kharraz02@correo.ugr.es>
2023-10-19 17:24:28 +0200

    Solucionado #7 closes #7
    Usando una clase para modelar este objeto valor

--- src/Atleta.ts
+++ src/Atleta.ts
@@ -1,3 +1,30 @@
+import { DisponibilidadHoraria } from "./DisponibilidadHoraria";
+
 export class Atelta{
     private _fechaNacimiento: number;
+    private _nivelRendimiento: Map<number, string>;
+    private _nivelCompromiso: Map<number, string>;
+    private _modalidadEntreno: Map<number, string>;
+    private _disponibilidadHorariaSemanal: DisponibilidadHoraria;
+    private _preferenciasContacto: Map<number, string>;
+    private _categoriasPeso: Map<string, [number, number]>;
+
+    constructor(
+        fechaNacimiento: number,
+        nivelRendimiento: Map<number, string>,
+        nivelCompromiso: Map<number, string>,
+        modalidadEntreno: Map<number, string>,
+        disponibilidadHorariaSemanal: DisponibilidadHoraria,
+        preferenciasContacto: Map<number, string>,
+        categoriasPeso: Map<string, [number, number]>) 
+    {
+        this._fechaNacimiento = fechaNacimiento;
+        this._nivelRendimiento = nivelRendimiento;
+        this._nivelCompromiso = nivelCompromiso;
+        this._modalidadEntreno = modalidadEntreno;
+        this._disponibilidadHorariaSemanal = disponibilidadHorariaSemanal;
+        this._preferenciasContacto = preferenciasContacto;
+        this._categoriasPeso = categoriasPeso;
+
+    }
 }

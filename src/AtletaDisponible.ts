ab6fa7efe818821d2ab902c24844b81857f22179
Morad Kharraz Senhaji <kharraz02@correo.ugr.es>
2023-10-19 18:15:28 +0200

    Solucionado #29 closes #29
    Creaod una clase entidad para gestionar el valor objeto atleta

---
+++ src/AtletaDisponible.ts
@@ -0,0 +1,25 @@
+import { Atelta } from "./Atleta";
+import { EntrenadorDisponible } from "./EntrenadorDisponible";
+import { DisponibilidadHoraria } from "./DisponibilidadHoraria";
+
+export class AtletaDisponible extends Atelta {
+    private _id: number;
+    private _entrenadoresSugeridos: EntrenadorDisponible[];
+    private _entrenadorElegido: EntrenadorDisponible;
+    private _disponibilidad: boolean;
+
+    constructor(
+        fechaNacimiento: number,
+        nivelRendimiento: Map<number, string>,
+        nivelCompromiso: Map<number, string>,
+        modalidadEntreno: Map<number, string>,
+        disponibilidadHorariaSemanal: DisponibilidadHoraria,
+        preferenciasContacto: Map<number, string>,
+        categoriasPeso: Map<string, [number, number]>,
+        id: number,
+        ) 
+    {
+        super(fechaNacimiento, nivelRendimiento, nivelCompromiso, modalidadEntreno, disponibilidadHorariaSemanal, preferenciasContacto, categoriasPeso);
+        this._id = id;
+    }
+}

8efa73ea0cb658feaaf61c51afb9924e7a5bc8f6
MarioGuisado <granadamariogg@gmail.com>
2023-11-24 22:53:51 +0100

    Corregido types.ts, closes #50

--- src/types.ts
+++ src/types.ts
@@ -1,38 +1,64 @@
-export const Rendimiento = new Map<string, number>([
-    ['BAJO', 0],
-    ['MEDIO', 0.5 ],
-    ['ALTO', 1 ],
-]);
 
-export const Compromiso = new Map<string, number>([
-    ['BAJO', 0],
-    ['MEDIO', 0.5],
-    ['ALTO', 1],
-]);
+export enum Rendimiento{
+    REGIONAL,
+    NACIONAL,
+    INTERNACIONAL, 
+};
 
-export const ModalidadEntreno = new Map<string, number>([
-    ['PRESENCIAL', 0],
-    ['SEMIPRESENCIAL', 0.5],
-    ['ONLINE', 1],
-]);
+export enum Compromiso{
+    BAJO,
+    MEDIO,
+    ALTO, 
+};
 
-export const TipoContacto = new Map<string, number>([
-    [ 'WHATSAPP',0],
-    ['TELEGRAM', 0.5],
-    ['EMAIL', 1 ],
-]);
+export enum ModalidadEntreno{
+    PRESENCIAL,
+    SEMIPRESENCIAL,
+    ONLINE, 
+};
 
-export enum CategoriasPeso {
-    MENOS_66,
-    MENOS_74,
-    MENOS_83,  
-    MENOS_93,
-    MENOS_105,
-    MENOS_120,
-    MAS_120,
+export enum TipoContacto{
+    WHATSAPP,
+    TELEGRAM,
+    EMAIL, 
 };
 
 export enum TipoMatriz{
     ENTRENADORES,
     ATLETAS
 }
+
+// export const Rendimiento = new Map<string, number>([
+//     ['BAJO', 0],
+//     ['MEDIO', 0.5 ],
+//     ['ALTO', 1 ],
+// ]);
+
+// export const Compromiso = new Map<string, number>([
+//     ['BAJO', 0],
+//     ['MEDIO', 0.5],
+//     ['ALTO', 1],
+// ]);
+
+// export const ModalidadEntreno = new Map<string, number>([
+//     ['PRESENCIAL', 0],
+//     ['SEMIPRESENCIAL', 0.5],
+//     ['ONLINE', 1],
+// ]);
+
+// export const TipoContacto = new Map<string, number>([
+//     [ 'WHATSAPP',0],
+//     ['TELEGRAM', 0.5],
+//     ['EMAIL', 1 ],
+// ]);
+
+// export enum CategoriasPeso {
+//     MENOS_66,
+//     MENOS_74,
+//     MENOS_83,  
+//     MENOS_93,
+//     MENOS_105,
+//     MENOS_120,
+//     MAS_120,
+// };
+

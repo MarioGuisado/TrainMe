2359ce8b9e0cc801ee4ab7f41ceac49ea3ce26ab
MarioGuisado <granadamariogg@gmail.com>
2023-11-24 22:51:58 +0100

    Corregida clase emparejador, closes #48

--- src/emparejador.ts
+++ src/emparejador.ts
@@ -1,62 +1,55 @@
-import { Entrenador } from './entrenador';
-import { Atleta } from './atleta';
 import { TipoMatriz} from './types';
+import { Usuario } from './usuario';
 
 
 export class Emparejador {
   matriz_entrenadores: number[][];
   matriz_atletas: number[][];
 
-  entrenadores: Entrenador[];
-  atletas: Atleta[];
-
   constructor() {
     this.matriz_entrenadores = [];
     this.matriz_atletas = [];
-    this.entrenadores = [];
-    this.atletas = [];
   }
 
-  procesarUsuario(usuario: Entrenador | Atleta): void{
+  ProcesarUsuario(usuario: Usuario, es_entrenador:boolean): void{
     const vector: number[] = [];
 
     vector.push(usuario.getid());
-
-
-    vector.push(usuario.getnivelRendimiento().values().next().value);
-    vector.push(usuario.getnivelCompromiso().values().next().value);
-    vector.push(usuario.getmodalidadEntreno().values().next().value);
-    vector.push(usuario.getpreferenciasContacto().values().next().value);
+    vector.push(usuario.getnivelRendimiento());
+    vector.push(usuario.getnivelCompromiso());
+    vector.push(usuario.getmodalidadEntreno());
+    vector.push(usuario.getpreferenciasContacto());
     
-    if(usuario instanceof Entrenador)
+    if(es_entrenador)
       this.matriz_entrenadores.push(vector);
     else
      this.matriz_atletas.push(vector);
     }
 
-  calculateSimilarity(usuario: number[], matriz_a_comparar: TipoMatriz): number {
-    let minDistance = Infinity;
-    let distance;
+  CalculateSimilarity(usuario: number[], matriz_a_comparar: TipoMatriz): number {
     let matriz;
-    let id_elegido = -1;
+    let id_elegido = usuario[0];
 
     if (matriz_a_comparar == TipoMatriz.ENTRENADORES)
       matriz = this.matriz_entrenadores;
     else
       matriz = this.matriz_atletas;
 
+    let puntuaciones: number[][] = [];
+    let distance;
+
     matriz.forEach((row) => {
-      distance = this.euclideanDistance(usuario, row);
-      if (distance < minDistance) {
-        minDistance = distance;
-        id_elegido = row[0];
-      }
+      distance = this.EuclideanDistance(usuario, row);
+      puntuaciones.push([row[0], distance]);
     });
+
+    puntuaciones.sort((a, b) => a[1] - b[1]);
+    id_elegido = puntuaciones[0][0];
     
     return id_elegido;
   }
 
-  getUsuario(id_elegido: number, matriz_a_comparar: TipoMatriz): number[] | undefined {
+  GetUsuario(id_elegido: number, matriz_a_comparar: TipoMatriz): number[] | undefined {
     let matriz: number[][];
 
     if (matriz_a_comparar == TipoMatriz.ENTRENADORES)
@@ -67,7 +60,7 @@ export class Emparejador {
     return matriz.find((row) => row[0] == id_elegido);
   }
 
-  euclideanDistance(vector1: number[], vector2: number[]): number {
+  EuclideanDistance(vector1: number[], vector2: number[]): number {
     if (vector1.length !== vector2.length) {
         throw new Error('Los vectores deben tener la misma longitud');
     }
@@ -83,24 +76,24 @@ export class Emparejador {
     return Math.sqrt(sum);  
   }
 
-  realizarEmparejamiento(usuario: Entrenador | Atleta): number {
+  RealizarEmparejamiento(usuario: Usuario, es_entrenador:boolean): number {
     let id_usuario = usuario.getid();
     let vector_usuario: number[] | undefined;
     let tipoMatriz: TipoMatriz;
 
-    if(usuario instanceof Entrenador)
+    if(es_entrenador)
       tipoMatriz = TipoMatriz.ENTRENADORES;  
     else
       tipoMatriz = TipoMatriz.ATLETAS;
 
-    vector_usuario = this.getUsuario(id_usuario, tipoMatriz);
-    let id_devuelto = -1
+    vector_usuario = this.GetUsuario(id_usuario, tipoMatriz);
+    let id_devuelto = id_usuario;
 
     if(vector_usuario != undefined){
-      if(tipoMatriz == TipoMatriz.ATLETAS)
-        id_devuelto = this.calculateSimilarity(vector_usuario, TipoMatriz.ENTRENADORES);
+      if(!es_entrenador)
+        id_devuelto = this.CalculateSimilarity(vector_usuario, TipoMatriz.ENTRENADORES);
       else
-        id_devuelto = this.calculateSimilarity(vector_usuario, TipoMatriz.ATLETAS);
+        id_devuelto = this.CalculateSimilarity(vector_usuario, TipoMatriz.ATLETAS);
     }
 
     return id_devuelto;

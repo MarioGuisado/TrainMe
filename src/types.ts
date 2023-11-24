12f88e45f4858617855e5479d4081cc0afc744da
MarioGuisado <granadamariogg@gmail.com>
2023-11-24 23:10:42 +0100

    Comentarios eliminado en types.ts para #50

--- src/types.ts
+++ src/types.ts
@@ -28,37 +28,3 @@ export enum TipoMatriz{
     ATLETAS
 }
 
-// export const Rendimiento = new Map<string, number>([
-//     ['BAJO', 0],
-//     ['MEDIO', 0.5 ],
-//     ['ALTO', 1 ],
-// ]);
-
-// export const Compromiso = new Map<string, number>([
-//     ['BAJO', 0],
-//     ['MEDIO', 0.5],
-//     ['ALTO', 1],
-// ]);
-
-// export const ModalidadEntreno = new Map<string, number>([
-//     ['PRESENCIAL', 0],
-//     ['SEMIPRESENCIAL', 0.5],
-//     ['ONLINE', 1],
-// ]);
-
-// export const TipoContacto = new Map<string, number>([
-//     [ 'WHATSAPP',0],
-//     ['TELEGRAM', 0.5],
-//     ['EMAIL', 1 ],
-// ]);
-
-// export enum CategoriasPeso {
-//     MENOS_66,
-//     MENOS_74,
-//     MENOS_83,  
-//     MENOS_93,
-//     MENOS_105,
-//     MENOS_120,
-//     MAS_120,
-// };
-
